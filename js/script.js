205163a7f4e62028c40e51557da3a9f87ea9242c
SuayMack <pry_chu@hotmail.com>
2023-06-01 20:14:49 -0300

    atualização tamanho do form e ajustes no código js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 function validarEmail() {
     // Obtém o valor do campo de e-mail
-  var email = document.getElementById('email').value;
-  
+  var email = document.getElementById('email').value;  
   // Expressão regular para validar o formato do e-mail
-  var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
+  var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;  
   // Valida o campo de e-mail
   if (regex.test(email)) {
-    alert("E-mail válido!");
+    return
   } else {
     alert("E-mail inválido!");
   }
@@ -15,27 +13,19 @@ function validarEmail() {
 
 function enviarFormulario() {
   // Impede o envio padrão do formulário
-  preventDefault();
   
   // Validação do formulário
   var nome = document.getElementById('nome').value;
   var email = document.getElementById('email').value;
   var mensagem = document.getElementById('mensagem').value;
-  
-  if (nome === '' || email === '' || mensagem === '') {
-    exibirMensagem('O(s) campo(s) não pode ficar em branco.');
-    return;
-  }
-  
+  validarEmail() 
+  if (nome == '' || email == '' || mensagem == ''){
+    alert('O(s) campo(s) não pode ficar em branco.');
+  } 
+
   // Se a validação for bem-sucedida, exibe mensagem de sucesso
-  exibirMensagem('successMessage', 'Formulário enviado com sucesso!');
+  alert('Formulário enviado com sucesso!');
   
   // Limpa o formulário após a submissão
   document.getElementById('formulario').reset();
-}
-
-function exibirMensagem(elementId, message) {
-  var element = document.getElementById(elementId);
-  element.innerText = message;
-  element.style.display = 'block';
 }
