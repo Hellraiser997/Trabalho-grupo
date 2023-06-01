ff8420f616567f61289659695b2c1910f853faca
VictorVechi <victorhvechi@gmail.com>
2023-06-01 09:19:29 -0300

    Remoção do DOM no javascript

--- js/servicos.js
+++ js/servicos.js
@@ -1,17 +1,28 @@
-var horaBaseReais
-var diaDaSemana
+var horaBaseReais, diaDaSemana, horaExtra, valorDaHora, qtdPessoas, valorDaHoraPessoa
 const adicionalPorMinuto = 0.50
-function calcularHora() {
-    diaDaSemana = Number(document.getElementById("dia-semana").value)
+function calcularHora(){
+    diaDaSemana = Number(prompt("Qual dia da semana pretende jogar:\nDigite 1 para Segunda-feira a Quinta-feira\nDigite 2 para Sexta-feira a Domingo"))
+    qtdPessoas = Number(prompt("Quantas pessoas irão jogar? (Digite apenas número)"))
+    if(qtdPessoas <= 0){
+        alert("Valor inválido")
+        calcularHora()
+    }
+    horaExtra = Number(prompt("Digite quantos minutos adicionais pretende ficar: 0 - 300"))
     console.log(diaDaSemana)
-    if(diaDaSemana <= 4){
+    if(diaDaSemana == 1){
         horaBaseReais = 45
     } else {
         horaBaseReais = 60
     }
-    var horaExtra = Number(document.getElementById("tempo-adicional").value)
-    var valorDaHora = horaBaseReais + (horaExtra*adicionalPorMinuto)
+    valorDaHora = horaBaseReais + (horaExtra*adicionalPorMinuto)
+    valorDaHoraPessoa = valorDaHora/qtdPessoas
     valorDaHora = valorDaHora.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
     horaBaseReais = horaBaseReais.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
-    alert("O valor da pista ficou em: "+valorDaHora+"\nSendo o valor base: "+horaBaseReais)
+    valorDaHoraPessoa = valorDaHoraPessoa.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
+
+    if(qtdPessoas == 1) {
+        alert("O valor da pista ficou em: "+valorDaHora+"\nSendo o valor base: "+horaBaseReais)
+    } else { 
+        alert("O valor da pista ficou em: "+valorDaHora+"\nSendo o valor base: "+horaBaseReais+"\nValor por pessoa: "+valorDaHoraPessoa)
+    }
 }
